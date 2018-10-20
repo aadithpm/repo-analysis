5b47d6101d527b9742b76bae2aaaf9bf82858ef6
aadithpm <aadithpm@gmail.com>
2018-10-19 21:19:20 -0500

    Added method to make API requests for building the dataset

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ def get_num_repositories(url):
 	c = BeautifulSoup(request.content, "lxml")
 	return int(c.find_all("span", "Counter")[0].contents[0])
 
-def get_users(id=0):
+def get_users(id = 0):
     if id == 0:
         request = requests.get("https://api.github.com/users", auth = (user, passw))
     else:
@@ -17,4 +17,17 @@ def get_users(id=0):
 
     json_data = json.loads(request.text)
     return json_data
+
+def make_dataset(id = 0):
+    users_data = []
+    since_id = id
+    while 1:
+        request_data = get_users(since_id)
+        try:
+            since_id = request_data[-1]['id']
+        except:
+            break
+        users_data.extend(request_data)
+        print(" -- Current size: {} -- Got {} users".format(str(len(users_data)), str(len(request_data))))
+    return users_data    
 
